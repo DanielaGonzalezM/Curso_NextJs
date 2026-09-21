ae8368057bcc1efe50a3fdb35faddaf56026202a
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2026-09-21 19:17:57 -0300

    Configurar Prettier + Husky + lint-staged para formateo automatico en commits

--- 02-my-dashboard/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/app/dashboard/counter/page.tsx
@@ -1,34 +1,16 @@
-"use client";
-import { Metadata } from "next";
-import { useState } from "react";
+import { CartCounter } from '@/app/shopping-cart';
+import { Metadata } from 'next';
 
-
-export const metadata:Metadata = {
- title: 'Counter Page',
- description: 'Un simple contador',
+export const metadata: Metadata = {
+  title: 'Shopping Cart',
+  description: 'Un simple contador',
 };
 
 export default function CounterPage() {
-  const [count, setCount] = useState(5);
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el carrito</span>
-      <span className="text-9xl">{count}</span>
-
-      <div className="flex">
-        <button
-          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-25 mr-2"
-          onClick={() => setCount(count + 1)}
-        >
-          +1
-        </button>
-        <button
-          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-25 mr-2"
-          onClick={() => setCount(count - 1)}
-        >
-          -1
-        </button>
-      </div>
+      <CartCounter value={20} />
     </div>
   );
 }
