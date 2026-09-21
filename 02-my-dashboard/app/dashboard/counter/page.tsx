3946a09aeb60f42df1f37ceab8bc760e758bcceb
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2026-09-21 19:03:32 -0300

    Counter - useState

--- 02-my-dashboard/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/app/dashboard/counter/page.tsx
@@ -1,8 +1,34 @@
+"use client";
+import { Metadata } from "next";
+import { useState } from "react";
+
+
+export const metadata:Metadata = {
+ title: 'Counter Page',
+ description: 'Un simple contador',
+};
 
 export default function CounterPage() {
+  const [count, setCount] = useState(5);
   return (
-    <div>
-      <h1>Hello CounterPage</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">{count}</span>
+
+      <div className="flex">
+        <button
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-25 mr-2"
+          onClick={() => setCount(count + 1)}
+        >
+          +1
+        </button>
+        <button
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-25 mr-2"
+          onClick={() => setCount(count - 1)}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
 }
